192c2d22d55aef2c84eb8da32158044e6a1f3413
Vasyl Vasiuk <vasyl.vasiuk@moonshiner.at>
2023-12-03 20:14:46 +0100

    feat: hide admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       </head>
       <body className={jost.variable}>
         <Providers>
-          <AdminBar />
+          {/* <AdminBar /> */}
           {/* @ts-expect-error */}
           <Header />
           <main className="main">{children}</main>
